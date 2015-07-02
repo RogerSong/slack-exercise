0f8fb8864077521e7574b324021288839cacd0c5
Roger Song <Roger.Song.NY@gmail.com>
2015-07-02 14:50:13 -0400

    Added min and max amount
    Min and max amount of exercise is user definable now

--- variables.php
+++ variables.php
@@ -5,6 +5,7 @@
 $token = "";
 
 // Channel to post the exercise
+// Don't forget to prefix the channel with a hashtag
 $channel = "#random";
 
 // Username of the bot
@@ -18,8 +19,11 @@ $exercise = array(
 	"sit ups",
 	"push ups",
 	"squats",
-	"jumping jacks",
-	"pak punches"
+	"jumping jacks"
 );
+
+// Generates a random number between $min_amount and $max_amount
+$min_amount = 10;
+$max_amount = 25;
 
 ?>
