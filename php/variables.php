a95ebc31f5a9c61f468d51447536dc1c2d00272e
Roger Song <Roger.Song.NY@gmail.com>
2015-07-06 15:51:58 -0400

    Moved from root to php folder

---
+++ php/variables.php
@@ -0,0 +1,29 @@
+<?php
+
+// Paste your Slack API token in here. You grab yours from: https://api.slack.com/web#authentication
+// It should look like this: xoxp-##########-##########-##########-######
+$token = "";
+
+// Channel to post the exercise
+// Don't forget to prefix the channel with a hashtag
+$channel = "#random";
+
+// Username of the bot
+$username = "ExerciseBot";
+
+// User icon of the bot
+$icon = ":muscle:";
+
+// List of exercises
+$exercise = array(
+    "sit ups",
+    "push ups",
+    "squats",
+    "jumping jacks"
+);
+
+// Generates a random number between $min_amount and $max_amount
+$min_amount = 10;
+$max_amount = 25;
+
+?>
